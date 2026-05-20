384362b791970433cf9655e8b9ebab349506cc9e
Gustavo Paulino <gustavo.paulino.cantanhede@gmail.com>
2026-05-20 16:11:18 -0300

    Initial commit

---
+++ src/main/java/br/com/bookschange/BookschangeApplication.java
@@ -0,0 +1,13 @@
+package br.com.bookschange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookschangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookschangeApplication.class, args);
+	}
+
+}
