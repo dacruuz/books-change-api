96056a87000412b38cffc3fb2549c9e6e63a1c80
Gustavo Paulino <gustavo.paulino.cantanhede@gmail.com>
2026-05-29 13:01:01 -0300

    # 0015 - feat: implementing cpf user validation.

--- src/main/java/br/com/bookschange/api/application/user/adapters/out/repositories/UserJpaRepository.java
+++ src/main/java/br/com/bookschange/api/application/user/adapters/out/repositories/UserJpaRepository.java
@@ -8,4 +8,5 @@ import java.util.UUID;
 
 @Repository
 public interface UserJpaRepository extends JpaRepository<User, UUID> {
+    boolean existsByCpf(String cpf);
 }
--- src/main/java/br/com/bookschange/api/application/user/usecases/CreateUserUseCase.java
+++ src/main/java/br/com/bookschange/api/application/user/usecases/CreateUserUseCase.java
@@ -5,9 +5,11 @@ import br.com.bookschange.api.application.user.adapters.in.dtos.response.CreateU
 import br.com.bookschange.api.application.user.mappers.UserMapper;
 import br.com.bookschange.api.application.user.ports.in.CreateUserPortIn;
 import br.com.bookschange.api.application.user.ports.out.CreateUserPortOut;
+import br.com.bookschange.api.application.user.ports.out.FindUserPortOut;
 import br.com.bookschange.api.domain.enums.UserType;
 import br.com.bookschange.api.domain.exceptions.BusinessException;
 import br.com.bookschange.api.domain.models.User;
+import br.com.bookschange.infrastructure.shared.util.CPFUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +19,12 @@ public class CreateUserUseCase implements CreateUserPortIn {
 
     private final UserMapper mapper;
     private final CreateUserPortOut createUserPortOut;
+    private final FindUserPortOut findUserPortOut;
 
     @Override
     public CreateUserResponse create(String userType, CreateUserRequest request) {
+        validateCpf(request);
+
         User user = mapper.createUserRequestToEntity(request);
         user.setActive(true);
 
@@ -28,5 +33,12 @@ public class CreateUserUseCase implements CreateUserPortIn {
 
         User createdUser = createUserPortOut.create(user);
         return mapper.toCreateUserResponse(createdUser);
+    }
+
+    private void validateCpf(CreateUserRequest request) {
+        String normalizedCpf = CPFUtil.normalize(request.cpf());
+        boolean cpfAlreadyExists = findUserPortOut.existsByCpf(normalizedCpf);
+
+        if (cpfAlreadyExists) throw new BusinessException("Já existe um usuário cadastrado com esse cpf");
     }
 }
