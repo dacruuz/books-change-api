f1d588bb9da2b535acc893bbce6efc5cea086241
dacruuz <gustavo.paulino.cantanhede@gmail.com>
2026-05-28 22:12:01 -0300

    # 0012 - feat: implementing find user by uuid endpoint.

--- src/main/java/br/com/bookschange/api/application/user/mappers/UserMapper.java
+++ src/main/java/br/com/bookschange/api/application/user/mappers/UserMapper.java
@@ -2,12 +2,14 @@ package br.com.bookschange.api.application.user.mappers;
 
 import br.com.bookschange.api.application.user.adapters.in.dtos.request.CreateUserRequest;
 import br.com.bookschange.api.application.user.adapters.in.dtos.response.CreateUserResponse;
+import br.com.bookschange.api.application.user.adapters.in.dtos.response.FindUserResponse;
 import br.com.bookschange.api.domain.models.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User createUserRequestToEntity(CreateUserRequest request);
-
     CreateUserResponse toCreateUserResponse(User user);
+
+    FindUserResponse toFindUserResponse(User user);
 }
