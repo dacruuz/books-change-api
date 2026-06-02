f73a44be6f585b60774c7ec32ec96bb031026d8c
Gustavo Paulino <gustavo.paulino.cantanhede@gmail.com>
2026-06-02 16:47:40 -0300

    # 0029 - feat: implementing find store by uuid endpoint and adding some validation in create store endpoint.

--- src/main/java/br/com/bookschange/api/application/store/adapters/in/StoreController.java
+++ src/main/java/br/com/bookschange/api/application/store/adapters/in/StoreController.java
@@ -3,14 +3,14 @@ package br.com.bookschange.api.application.store.adapters.in;
 import br.com.bookschange.api.application.store.adapters.in.dtos.request.CreateStoreRequest;
 import br.com.bookschange.api.application.store.adapters.in.dtos.response.StoreResponse;
 import br.com.bookschange.api.application.store.ports.in.CreateStorePortIn;
+import br.com.bookschange.api.application.store.ports.in.FindStorePortIn;
 import br.com.bookschange.infrastructure.shared.ApiResponseBuilder;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/store")
@@ -19,10 +19,17 @@ public class StoreController {
 
     private final ApiResponseBuilder apiResponseBuilder;
     private final CreateStorePortIn createStorePortIn;
+    private final FindStorePortIn findStorePortIn;
 
     @PostMapping
     public ResponseEntity<?> create(@Valid @RequestBody CreateStoreRequest request) {
         StoreResponse response = createStorePortIn.create(request);
         return apiResponseBuilder.buildCreated(response);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<?> findByUuid(@PathVariable UUID uuid) {
+        StoreResponse response = findStorePortIn.findByUuid(uuid);
+        return apiResponseBuilder.buildSuccess(response);
     }
 }
