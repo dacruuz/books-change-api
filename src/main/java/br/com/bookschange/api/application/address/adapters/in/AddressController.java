d3b2e7009c3fdecc3eee5b60d04bc9fbeed700be
dacruuz <gustavo.paulino.cantanhede@gmail.com>
2026-06-02 22:54:13 -0300

    # 0033 - feat: implementing find address by uuid endpoint.

--- src/main/java/br/com/bookschange/api/application/address/adapters/in/AddressController.java
+++ src/main/java/br/com/bookschange/api/application/address/adapters/in/AddressController.java
@@ -3,14 +3,14 @@ package br.com.bookschange.api.application.address.adapters.in;
 import br.com.bookschange.api.application.address.adapters.in.dtos.request.CreateAddressRequest;
 import br.com.bookschange.api.application.address.adapters.in.dtos.response.AddressResponse;
 import br.com.bookschange.api.application.address.ports.in.CreateAddressPortIn;
+import br.com.bookschange.api.application.address.ports.in.FindAddressPortIn;
 import br.com.bookschange.infrastructure.shared.ApiResponseBuilder;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/address")
@@ -19,10 +19,17 @@ public class AddressController {
 
     private final ApiResponseBuilder apiResponseBuilder;
     private final CreateAddressPortIn createAddressPortIn;
+    private final FindAddressPortIn findAddressPortIn;
 
     @PostMapping
     public ResponseEntity<?> create(@Valid @RequestBody CreateAddressRequest request) {
         AddressResponse response = createAddressPortIn.create(request);
         return apiResponseBuilder.buildCreated(response);
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<?> findByUuid(@PathVariable UUID uuid) {
+        AddressResponse response = findAddressPortIn.findByUuid(uuid);
+        return apiResponseBuilder.buildSuccess(response);
     }
 }
