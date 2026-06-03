31993e6cba4e1f8bb0db5012d1501501431465b2
dacruuz <gustavo.paulino.cantanhede@gmail.com>
2026-06-02 21:18:44 -0300

    # 0030 - feat: implementing store validator and normalizer.

--- src/main/java/br/com/bookschange/api/application/store/usecases/CreateStoreUseCase.java
+++ src/main/java/br/com/bookschange/api/application/store/usecases/CreateStoreUseCase.java
@@ -4,12 +4,11 @@ import br.com.bookschange.api.application.store.adapters.in.dtos.request.CreateS
 import br.com.bookschange.api.application.store.adapters.in.dtos.response.StoreResponse;
 import br.com.bookschange.api.application.store.mappers.StoreMapper;
 import br.com.bookschange.api.application.store.ports.in.CreateStorePortIn;
-import br.com.bookschange.api.application.store.ports.out.FindStorePortOut;
 import br.com.bookschange.api.application.store.ports.out.SaveStorePortOut;
-import br.com.bookschange.api.domain.exceptions.BusinessException;
+import br.com.bookschange.api.application.store.services.StoreNormalizer;
+import br.com.bookschange.api.application.store.services.StoreValidator;
 import br.com.bookschange.api.domain.models.Store;
 import br.com.bookschange.infrastructure.shared.util.CNPJUtil;
-import br.com.bookschange.infrastructure.shared.util.PhoneUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,8 +19,9 @@ import org.springframework.stereotype.Service;
 public class CreateStoreUseCase implements CreateStorePortIn {
 
     private final StoreMapper mapper;
+    private final StoreValidator validator;
+    private final StoreNormalizer normalizer;
     private final SaveStorePortOut saveStorePortOut;
-    private final FindStorePortOut findStorePortOut;
 
     @Override
     public StoreResponse create(CreateStoreRequest request) {
@@ -29,10 +29,11 @@ public class CreateStoreUseCase implements CreateStorePortIn {
                 CNPJUtil.format(request.cnpj()),
                 request.commercialEmail());
 
-        validadeDate(request);
+        validator.validateCreation(request.commercialEmail(), request.cnpj(), request.slug());
 
         Store store = mapper.createStoreRequestToEntity(request);
-        normalizeData(store);
+
+        normalizer.normalize(store);
 
         Store createdStore = saveStorePortOut.save(store);
 
@@ -42,48 +43,5 @@ public class CreateStoreUseCase implements CreateStorePortIn {
                 createdStore.getCommercialEmail());
 
         return mapper.toStoreResponse(createdStore);
-    }
-
-    private void normalizeData(Store store) {
-        store.setCnpj(CNPJUtil.normalize(store.getCnpj()));
-        store.setPhone(PhoneUtil.normalize(store.getPhone()));
-        store.setCommercialEmail(store.getCommercialEmail().trim().toLowerCase());
-        store.setSlug(store.getSlug().trim().toLowerCase());
-    }
-
-    private void validadeDate(CreateStoreRequest request) {
-        validateEmail(request.commercialEmail());
-        validateCpnj(request.cnpj());
-        validateSlug(request.slug());
-    }
-
-    private void validateSlug(String slug) {
-        String normalizedSlug = slug.trim().toLowerCase();
-        boolean slugAlreadyExists = findStorePortOut.existsBySlug(normalizedSlug);
-
-        if (slugAlreadyExists) {
-            log.warn("Tentativa de cadastro com identificador existente");
-            throw new BusinessException("Já existe uma loja cadastrada com esse identificador");
-        }
-    }
-
-    private void validateEmail(String email) {
-        String normalizedEmail = email.trim().toLowerCase();
-        boolean emailAlreadyExists = findStorePortOut.existsByEmail(normalizedEmail);
-
-        if (emailAlreadyExists) {
-            log.warn("Tentativa de cadastro com e-mail existente");
-            throw new BusinessException("Já existe uma loja cadastrada com esse e-mail");
-        }
-    }
-
-    private void validateCpnj(String cnpj) {
-        String normalizedCnpj = CNPJUtil.normalize(cnpj);
-        boolean cnpjAlreadyExists = findStorePortOut.existsByCnpj(normalizedCnpj);
-
-        if (cnpjAlreadyExists) {
-            log.warn("Tentativa de cadastro com CNPJ existente");
-            throw new BusinessException("Já existe uma loja cadastrada com esse CNPJ");
-        }
     }
 }
