3a9797ab922e5225a778a5d292e9ccb9078f67e5
dacruuz <gustavo.paulino.cantanhede@gmail.com>
2026-07-18 15:09:15 -0300

    feat: adding mapper to book filter.

--- src/main/java/br/com/bookschange/api/application/book/mappers/BookMapper.java
+++ src/main/java/br/com/bookschange/api/application/book/mappers/BookMapper.java
@@ -1,8 +1,10 @@
 package br.com.bookschange.api.application.book.mappers;
 
 import br.com.bookschange.api.application.book.adapters.in.dtos.request.CreateBookRequest;
+import br.com.bookschange.api.application.book.adapters.in.dtos.request.FilterBookRequest;
 import br.com.bookschange.api.application.book.adapters.in.dtos.request.UpdateBookRequest;
 import br.com.bookschange.api.application.book.adapters.in.dtos.response.BookResponse;
+import br.com.bookschange.api.application.book.dtos.BookFilter;
 import br.com.bookschange.api.domain.models.Book;
 import br.com.bookschange.api.domain.models.BookCategory;
 import br.com.bookschange.api.shared.dtos.SelectOptionDTO;
@@ -20,18 +22,20 @@ public interface BookMapper {
     @Mapping(target = "bookCategories", ignore = true)
     void updateBookFromRequest(UpdateBookRequest request, @MappingTarget Book book);
 
+    BookFilter filterBookRequestToBookFilter(FilterBookRequest request);
+
     @Mapping(target = "ownerUuid", source = "owner.uuid")
     @Mapping(target = "categories", source = "bookCategories")
     BookResponse entityToBookResponse(Book book);
 
     default List<SelectOptionDTO> mapBookCategories(List<BookCategory> bookCategories) {
         return bookCategories
-                .stream()
-                .map(bookCategory ->
-                        new SelectOptionDTO(
-                                bookCategory.getCategory().getUuid(),
-                                bookCategory.getCategory().getLabel()
-                        ))
-                .toList();
+            .stream()
+            .map(bookCategory ->
+                new SelectOptionDTO(
+                    bookCategory.getCategory().getUuid(),
+                    bookCategory.getCategory().getLabel()
+                ))
+            .toList();
     }
 }
