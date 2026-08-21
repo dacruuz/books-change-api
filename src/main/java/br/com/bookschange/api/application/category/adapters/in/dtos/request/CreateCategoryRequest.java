71c7131b22ac2e8562afb3a5a4d72600aaefd726
dacruuzz <gustavo.paulino.cantanhede@gmail.com>
2026-08-21 11:19:42 -0300

    # 0061 - fix(category): correcting label validation error message.

--- src/main/java/br/com/bookschange/api/application/category/adapters/in/dtos/request/CreateCategoryRequest.java
+++ src/main/java/br/com/bookschange/api/application/category/adapters/in/dtos/request/CreateCategoryRequest.java
@@ -3,7 +3,7 @@ package br.com.bookschange.api.application.category.adapters.in.dtos.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateCategoryRequest(
-        @NotBlank(message = "A label é obrigatória")
+        @NotBlank(message = "O nome da categoria é obrigatório")
         String label,
 
         @NotBlank(message = "O identificador é obrigatório")
