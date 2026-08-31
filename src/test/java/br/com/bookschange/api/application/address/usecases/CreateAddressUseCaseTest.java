5cc00f4e11606997ea6250f99b4f7cbb44d8d2dd
dacruuzz <gustavo.paulino.cantanhede@gmail.com>
2026-08-31 17:01:36 -0300

    # 0055 - test(address): adding address successfully unit test.

---
+++ src/test/java/br/com/bookschange/api/application/address/usecases/CreateAddressUseCaseTest.java
@@ -0,0 +1,83 @@
+package br.com.bookschange.api.application.address.usecases;
+
+import br.com.bookschange.api.application.address.adapters.in.dtos.request.CreateAddressRequest;
+import br.com.bookschange.api.application.address.adapters.in.dtos.response.AddressResponse;
+import br.com.bookschange.api.application.address.mappers.AddressMapper;
+import br.com.bookschange.api.application.address.ports.out.SaveAddressPortOut;
+import br.com.bookschange.api.application.address.services.AddressNormalizer;
+import br.com.bookschange.api.application.address.services.AddressValidator;
+import br.com.bookschange.api.domain.models.Address;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CreateAddressUseCaseTest {
+
+    @Mock private AddressMapper mapper;
+    @Mock private AddressNormalizer normalizer;
+    @Mock private AddressValidator validator;
+    @Mock private SaveAddressPortOut saveAddressPortOut;
+
+    @InjectMocks
+    CreateAddressUseCase useCase;
+
+    private CreateAddressRequest request;
+    private Address address;
+
+    @BeforeEach
+    void setUp() {
+        request = new CreateAddressRequest(
+                "70680-642",
+                "street",
+                "000",
+                "state",
+                "country",
+                "city",
+                "complement",
+                "neighborhood"
+        );
+
+        address = new Address();
+        address.setZipCode(request.zipCode());
+        address.setStreet(request.street());
+        address.setNumber(request.number());
+        address.setState(request.state());
+        address.setCountry(request.country());
+        address.setComplement(request.complement());
+        address.setNeighborhood(request.neighborhood());
+    }
+
+    @Test
+    @DisplayName("Deve criar um endereço com sucesso")
+    void shouldCreateNewAddressSuccessfully() {
+        doNothing().when(validator).validateZipCode(request.zipCode());
+        when(mapper.createAddressRequestToEntity(request)).thenReturn(address);
+        doNothing().when(normalizer).normalizeData(address);
+        when(saveAddressPortOut.save(address)).thenReturn(address);
+
+        AddressResponse expectedResponse = mock(AddressResponse.class);
+        when(mapper.entityToAddressResponse(address)).thenReturn(expectedResponse);
+
+        AddressResponse result = useCase.create(request);
+
+        assertEquals(expectedResponse, result);
+
+        ArgumentCaptor<Address> addressCaptor = ArgumentCaptor.forClass(Address.class);
+        verify(saveAddressPortOut).save(addressCaptor.capture());
+
+        verify(normalizer, times(1)).normalizeData(address);
+        verify(validator, times(1)).validateZipCode(anyString());
+        verify(mapper, times(1)).createAddressRequestToEntity(any());
+        verify(mapper, times(1)).entityToAddressResponse(any());
+        verify(saveAddressPortOut).save(address);
+    }
+}
